51a74e1310e63544969c71bda2f57ca771a73223
madu munachi <madustanley1@gmail.com>
2020-08-16 13:05:50 +0100

    muna

---
+++ src/components/Portfolio/Portfoliodata.js
@@ -0,0 +1,23 @@
+export const PortfolioData = [
+    {
+        id: 1,
+        head: 'Immaculate.com',
+        link: ''
+    },
+
+    {
+        id: 2,
+        head: 'Justice',
+        link: ''
+    },
+    {
+        id: 3,
+        head: 'Chantals',
+        link: '',
+    },
+    {
+        id: 4,
+        head: 'Portfolio',
+        link: '',
+    }
+]
