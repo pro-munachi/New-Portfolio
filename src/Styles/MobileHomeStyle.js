267587b5050974ca95eb1ce4a709443890e12be8
madu munachi <madustanley1@gmail.com>
2021-01-17 14:38:52 +0100

    mobile home

--- src/Styles/MobileHomeStyle.js
+++ src/Styles/MobileHomeStyle.js
@@ -2,19 +2,16 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   color: #777778;
-  height: auto;
-  padding-bottom: 14%;
   height: 100vh;
   .home {
     width: 90%;
     margin: auto;
     margin-top: 5%;
     padding-top: 10%;
-    padding-bottom: 12%;
     text-align: center;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    justify-content: center;
     .image {
       text-align: center;
       margin: 10px 0;
@@ -26,38 +23,44 @@ export const Container = styled.div`
     .intro {
       margin: 10px 0;
       h1 {
-        font-size: 34px;
+        font-size: 36px;
       }
       p {
-        font-size: 17px;
+        font-size: 19px;
       }
     }
-    .social {
-      margin: 10px 0;
-      .social-link {
-        display: flex;
-        p {
-          font-size: 18px;
-          margin: 8px 0 8px 8px;
-          a {
-            text-decoration: none;
-            color: white;
+    .footer {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .social {
+        margin: 10px 0;
+        .social-link {
+          display: flex;
+          p {
+            font-size: 18px;
+            margin: 8px 0 8px 8px;
+            a {
+              text-decoration: none;
+              color: white;
+            }
           }
         }
       }
-    }
-    .inspire {
-      margin: 18px 0;
-      font-size: 18px;
-      color: #777778;
-      a {
-        color: white;
-        margin: 8px;
+      .inspire {
+        margin: 18px 0;
+        font-size: 19px;
+        color: #777778;
+        a {
+          color: white;
+          margin: 8px;
+        }
       }
-    }
-    .copy {
-      color: white;
-      margin-top: 8px;
+      .copy {
+        color: white;
+        margin-top: 8px;
+        font-size: 19px;
+      }
     }
   }
 `
